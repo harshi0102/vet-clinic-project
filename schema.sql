fce1a367c0dc83cc28c275f579aa0c24d7058dd6
unknown <harshika0102@gmail.com>
2023-06-13 21:26:43 +0530

    Added sql code for updating animal table queries

--- schema.sql
+++ schema.sql
@@ -1,11 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id                 INT,
-    name               VARCHAR(100),
-    date_of_birth      DATE,
-    escape_attempts    INT,
-    neutered           BOOLEAN,
-    weight_kg          DECIMAL,
-    PRIMARY KEY(id)
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(300) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts INTEGER NOT NULL,
+    neutered BOOLEAN NOT NULL,
+    weight_kg DECIMAL NOT NULL
 );
+
+ALTER TABLE animals ADD COLUMN species VARCHAR(100);

2ebb565777f1dca47a751b014ef4e62aaea7dea6
unknown <harshika0102@gmail.com>
2023-06-19 22:56:10 +0530

    Fixed queries errors

--- data.sql
+++ data.sql
@@ -249,7 +249,6 @@ INSERT INTO visits (animals_id, vet_id,  date_of_visit)
       '11-JAN-2021'
     );
 
--- Add an email column to your owners table
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
 
 -- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
@@ -257,13 +256,12 @@ ALTER TABLE visits ADD COLUMN animal_id INT;
 
 INSERT INTO visits (animals_id, vet_id, date_of_visit)
 SELECT animal_ids.id, vets_ids.id, visit_timestamp
-FROM (SELECT id FROM animals WHERE id IS NOT NULL) animal_ids,
-     (SELECT id FROM vets) vets_ids,
+FROM (SELECT id FROM animals) AS animal_ids,
+     (SELECT id FROM vets) AS vets_ids,
      generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') AS visit_timestamp;
 
 
 -- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
-ALTER TABLE owners ALTER COLUMN age SET DEFAULT 0;
 
 insert into owners (full_name, email) 
 select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
